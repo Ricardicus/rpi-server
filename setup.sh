9a1f006b871463f864972bb8e24ec956a39b8494
Rickard <rickard.hallerback@gmail.com>
2020-09-25 15:20:49 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+MY_GITHUB="https://github.com/Ricardicus"
+
+function clone_repo {
+  echo "Getting project: $1"
+  if [ -d $1 ]; then
+    rm -rf $1
+  fi
+  git clone $MY_GITHUB/$1
+
+}
+
 # Checking git
 if [ -z $(which git) ]; then
   echo git needs to be installed >&2
@@ -13,10 +24,10 @@ if [ -z $(which docker-compose) ]; then
 fi
 
 # Cloning the projects I want
-git clone https://github.com/Ricardicus/count-down
-git clone https://github.com/Ricardicus/apache2-container
-git clone https://github.com/Ricardicus/ric-script
-git clone https://github.com/Ricardicus/virtualPiano
+clone_repo count-down
+clone_repo apache2-container
+clone_repo ric-script
+clone_repo virtualPiano
 
 # Build the images
 docker-compose build
